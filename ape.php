bbfbb87c6d75f3775e02a07100ce43a13d81a08a
isell06 <naomiangelia25@gmail.com>
2023-08-11 14:17:37 +0700

    update

--- ape.php
+++ ape.php
@@ -1,11 +1,11 @@
 <?php
     class Ape extends Animal
     {
-            public $yell;
-            public function get_yell() 
-            {
-                echo "Yell : ";
-                return $this->yell = "Auooo" . "<br>";
-            }
+        public $yell;
+        public function get_yell() 
+        {
+            echo "Yell : ";
+            return $this->yell = "Auooo" . "<br>";
+        }
     }
 ?>
